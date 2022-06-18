09b21147f4bcf7973dc58ed03d2d7b9598954bb6
Haruki Yano <haruki.y.11g@gmail.com>
2022-06-18 13:20:11 +0900

    Add UseAssetNameAsKey option to PreloadedAssetLoaderObject.

---
+++ Assets/UnityScreenNavigator/Runtime/Foundation/AssetLoader/Editor/PreloadedAssetObjectKeyAssetPairPropertyDrawer.cs
@@ -0,0 +1,70 @@
+﻿using System.Collections.Generic;
+using UnityEditor;
+using UnityEngine;
+
+namespace UnityScreenNavigator.Runtime.Foundation.AssetLoader.Editor
+{
+    [CustomPropertyDrawer(typeof(PreloadedAssetLoaderObject.KeyAssetPair))]
+    internal sealed class PreloadedAssetObjectKeyAssetPairPropertyDrawer : PropertyDrawer
+    {
+        private readonly Dictionary<string, PropertyData> _dataList = new Dictionary<string, PropertyData>();
+        private PropertyData _property;
+
+        private void Init(SerializedProperty property)
+        {
+            if (_dataList.TryGetValue(property.propertyPath, out _property)) return;
+
+            _property = new PropertyData
+            {
+                UseAssetNameAsKeyProperty = property.FindPropertyRelative("_useAssetNameAsKey"),
+                KeyProperty = property.FindPropertyRelative("_key"),
+                AssetProperty = property.FindPropertyRelative("_asset")
+            };
+            _dataList.Add(property.propertyPath, _property);
+        }
+
+        public override void OnGUI(Rect position, SerializedProperty property, GUIContent label)
+        {
+            Init(property);
+            var fieldRect = position;
+            fieldRect.height = EditorGUIUtility.singleLineHeight;
+
+            using (new EditorGUI.PropertyScope(fieldRect, label, property))
+            {
+                property.isExpanded = EditorGUI.Foldout(new Rect(fieldRect), property.isExpanded, label, true);
+                if (property.isExpanded)
+                    using (new EditorGUI.IndentLevelScope())
+                    {
+                        fieldRect.y += EditorGUIUtility.singleLineHeight + EditorGUIUtility.standardVerticalSpacing;
+                        EditorGUI.PropertyField(new Rect(fieldRect), _property.UseAssetNameAsKeyProperty);
+
+                        GUI.enabled = !_property.UseAssetNameAsKeyProperty.boolValue;
+                        fieldRect.y += EditorGUIUtility.singleLineHeight + EditorGUIUtility.standardVerticalSpacing;
+                        EditorGUI.PropertyField(new Rect(fieldRect), _property.KeyProperty);
+                        GUI.enabled = true;
+
+                        fieldRect.y += EditorGUIUtility.singleLineHeight + EditorGUIUtility.standardVerticalSpacing;
+                        EditorGUI.PropertyField(new Rect(fieldRect), _property.AssetProperty);
+                    }
+            }
+        }
+
+        public override float GetPropertyHeight(SerializedProperty property, GUIContent label)
+        {
+            Init(property);
+            var height = EditorGUIUtility.singleLineHeight;
+
+            if (property.isExpanded)
+                height += (EditorGUIUtility.singleLineHeight + EditorGUIUtility.standardVerticalSpacing) * 3;
+
+            return height;
+        }
+
+        private class PropertyData
+        {
+            public SerializedProperty AssetProperty;
+            public SerializedProperty KeyProperty;
+            public SerializedProperty UseAssetNameAsKeyProperty;
+        }
+    }
+}
--- Assets/UnityScreenNavigator/Runtime/Foundation/AssetLoader/PreloadedAssetLoaderObject.cs
+++ Assets/UnityScreenNavigator/Runtime/Foundation/AssetLoader/PreloadedAssetLoaderObject.cs
@@ -16,8 +16,27 @@ namespace UnityScreenNavigator.Runtime.Foundation.AssetLoader
 
         private void OnEnable()
         {
+            if (!Application.isPlaying)
+                return;
+
             foreach (var preloadedObject in _preloadedObjects)
+            {
+                if (string.IsNullOrEmpty(preloadedObject.Key))
+                    continue;
+
+                if (_loader.PreloadedObjects.ContainsKey(preloadedObject.Key))
+                    continue;
+
                 _loader.PreloadedObjects.Add(preloadedObject.Key, preloadedObject.Asset);
+            }
+        }
+
+        private void OnDisable()
+        {
+            if (!Application.isPlaying)
+                return;
+
+            _loader.PreloadedObjects.Clear();
         }
 
         public override AssetLoadHandle<T> Load<T>(string key)
@@ -36,14 +55,21 @@ namespace UnityScreenNavigator.Runtime.Foundation.AssetLoader
         }
 
         [Serializable]
-        public class KeyAssetPair
+        public sealed class KeyAssetPair
         {
+            [SerializeField] private bool _useAssetNameAsKey;
             [SerializeField] private string _key;
             [SerializeField] private Object _asset;
 
+            public bool UseAssetNameAsKey
+            {
+                get => _useAssetNameAsKey;
+                set => _useAssetNameAsKey = value;
+            }
+
             public string Key
             {
-                get => _key;
+                get => GetKey();
                 set => _key = value;
             }
 
@@ -51,6 +77,13 @@ namespace UnityScreenNavigator.Runtime.Foundation.AssetLoader
             {
                 get => _asset;
                 set => _asset = value;
+            }
+
+            private string GetKey()
+            {
+                if (_useAssetNameAsKey)
+                    return _asset == null ? "" : _asset.name;
+                return _key;
             }
         }
     }
